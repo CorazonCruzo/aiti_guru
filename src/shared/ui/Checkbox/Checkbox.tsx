f6cd1dd6f9636477651b2323b6c8ca65563e685d
lalitel <pity164@cancer-treatment.xyz>
2026-02-12 17:07:26 +0300

    add Checkbox component

---
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, type InputHTMLAttributes } from "react";
+import cn from "classnames";
+import { Check } from "@phosphor-icons/react";
+
+import styles from "./Checkbox.module.css";
+
+interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "type"> {
+    label?: string;
+}
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+    ({ label, className, ...rest }, ref) => {
+        return (
+            <label className={cn(styles.wrapper, className)}>
+                <input ref={ref} type="checkbox" className={styles.input} {...rest} />
+                <span className={styles.checkmark}>
+                    <Check size={14} weight="bold" className={styles.checkIcon} />
+                </span>
+                {label && <span className={styles.label}>{label}</span>}
+            </label>
+        );
+    },
+);
+
+Checkbox.displayName = "Checkbox";
